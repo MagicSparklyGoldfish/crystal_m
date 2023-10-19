3caa2c850ed1ed74320acfeae18c84d8e0303347
MagicSparklyGoldfish <whydeargodwhy@yahoo.com~>
2023-10-19 14:38:13 -0400

    made a module

--- src/Menus.cr
+++ src/Menus.cr
@@ -1,31 +1,13 @@
-require "crsfml"
-require "../src/Menus.cr"
-
-module CrystalMeth
-  VERSION = "0.1.0"
-  @[Link("GL")] # Use @[Link(framework: "OpenGL")] on Mac OSX
-lib GL
-  fun enable = glEnable(cap : Int32)
-    fun viewport = glViewport(x : Int32, y : Int32, width : Int32, height : Int32)
-      fun clear = glClear(mask : Int32)
-      TEXTURE_2D       =  3553
-      COLOR_BUFFER_BIT = 16384
-      DEPTH_BUFFER_BIT =   256
-end
-
 FONT_TITLE = SF::Font.from_file("fonts/PermanentMarker-Regular.ttf")
 FONT_COMMON = SF::Font.from_file("fonts/Changa/Changa-VariableFont_wght.ttf")
 FONT_FUTURE = SF::Font.from_file("fonts/Orbitron/Orbitron-VariableFont_wght.ttf")
 
-# it works out of the box
-GL.enable(GL::TEXTURE_2D)
-
-
-  def self.run
-    # Create a window
-    window = SF::RenderWindow.new(SF::VideoMode.new(1100, 900), "Crystal Meth!", SF::Style::Fullscreen)
-    window.vertical_sync_enabled = true 
-    #create title text
+#Main Menu Gui.Menus.drawmainmenu(window)
+module Gui
+class Menus
+        def Menus.drawmainmenu(this)
+            #`public`
+            #create title text
     text_title = SF::Text.new
     text_title.font = FONT_TITLE
     text_title.string = "Crystal Meth!"
@@ -65,35 +47,14 @@ GL.enable(GL::TEXTURE_2D)
     text_opt2.color = SF::Color::White
     text_opt2.style = (SF::Text::Bold)
     text_opt2.position = SF.vector2(880, 730)
- 
-
-    # Main loop: run the program as long as the window is open
-    while window.open?
-      # Check all the window's events that were triggered since the last iteration of the loop
-      while event = window.poll_event
-
-        
-
-        if event.is_a? SF::Event::KeyPressed
-          if event.code == SF::Keyboard::Escape
-            SF::Event::Closed
-            window.close
-          end
+    this.clear(SF::Color::Black)
+this.draw(text_title)
+this.draw(rectangle_menu)
+this.draw(rectangle_opt1)
+this.draw(text_opt1)
+this.draw(rectangle_opt2)
+this.draw(text_opt2)
         end
-        # "close requested" event: we close the window
-        if event.is_a? SF::Event::Closed
-          window.close
-        end
-      end
-      #draw stuff under here v
-      this = window
-      Gui::Menus.drawmainmenu(window)
-
-      window.display
     end
-  end
 end
-
-# Run the program
-CrystalMeth.run
 
